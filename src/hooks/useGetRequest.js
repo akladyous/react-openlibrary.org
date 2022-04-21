fe457a0a3eb5a95a308a313ee80b766c69e6d950
Boula Akladyous <akladyous@gmail.com>
2022-04-21 13:16:38 -0400

    fetch openlibrary using axios

--- src/hooks/useGetRequest.js
+++ src/hooks/useGetRequest.js
@@ -1,47 +1,54 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function useGetRequest(props) {
-    const {url} = props || {};
+export default function useGetRequest(query, pageNumber) {
+    // const {url} = props || {};
+
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [stateMessage, setStateMessage] = useState(null);
     const [data, setData] = useState([]);
 
-
     useEffect(() =>{
+        console.log("useEffect -----------------")
+        setLoading(true);
+        // if(query.length === 0) return;
         const controller = new AbortController();
-        (async () => {
-            setLoading(true)
-            const config = {
-                headers: { "Content-type": "application/json" },
-                signal: controller.signal,
-            };
-            const response = await axios.get(url, config);
-            return response
-        })()
+        const config = {
+            method: "GET",
+            signal: controller.signal,
+            url: "http://openlibrary.org/search.json",
+            headers: { "Content-type": "application/json" },
+            params: { q: query, page: pageNumber },
+        };
+        axios(config)
         .then(response => {
+            setStateMessage("completed")
+            setData(response.data)
             setLoading(false)
-            if(response && response.status === 200){
-                setStateMessage("completed")
-                setData(response.data)
-            } else {
-                setError(true)
-                setStateMessage("error")
-            };
         })
         .catch(error =>{
-            setLoading(false)
-            setError(true)
-            if(error.resonse.data.hasOwnProperty("error")){ //backend error / internal error
-                setStateMessage(error.response.data.error)
+            if(axios.isCancel(error)){
+                setStateMessage("http request canceled")
             } else {
-                setStateMessage(error.message) //query backend error
-            }
+                if(error.resonse.data.hasOwnProperty("error")){ //backend error / internal error
+                    setStateMessage(error.response.data.error)
+                } else {
+                    setStateMessage(error.message) //query backend error
+                }
+            }; // END - http request canceled
+            setError(true)
         })
-        .finally(() => {
+        // .finally(() => {
+        //     setLoading(false)
+        //     controller.abort()
+        // });
+        
+        return ()=>{ 
+            setLoading(false);
             controller.abort()
-        })
-    }, [] );
+        }
+    }, [query, pageNumber] );
+
     return {loading, error, stateMessage, data};
 };
